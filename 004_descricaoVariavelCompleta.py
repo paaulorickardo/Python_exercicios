2b61d26486c77240f1b5860e7a8365e4670a8f70
paaulorickardo <paaulorickardo@hotmail.com>
2021-12-29 08:13:23 -0300

    Aula 04-Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo primitivo e todas as informações possíveis sobre ele

---
+++ 004_descricaoVariavelCompleta.py
@@ -0,0 +1,13 @@
+'''Faça um programa que leia algo pelo teclado e mostre na tela o seu tipo
+primitivo e todas as informações possíveis sobre ele'''
+
+#Versão da Aula
+a = input('Digite algo: ')
+print('O tipo primitivo desse valor é: ', type(a))
+print('Só tem espaço? ', a.isspace())
+print('É um número? ', a.isnumeric())
+print('É alfabético? ', a.isalpha())
+print('É alfanumérico? ', a.isalnum())
+print('Está em maiúsculas? ', a.isupper())
+print('Está em minúsculas? ', a.islower())
+print('Esta letra maiscula a minusculas(capitalizada)? ', a.istitle)
