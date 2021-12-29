ef5baa711a285daff68831ceae62a2bc523f8bdd
paaulorickardo <paaulorickardo@hotmail.com>
2021-12-29 10:21:57 -0300

    Aula 14- Conversor de temperatura

---
+++ 014_conversorTemperatura.py
@@ -0,0 +1,5 @@
+
+#Versão aula
+c = float(input('Informe a temperatura em °C: '))
+f = ((9 * c)/5) + 32 #pode sem os paranteses, ordem de precedencia
+print('A temperatura de {}°C corresponde a {}°F!'.format(c, f))
