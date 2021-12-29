4ba188ffe577f20476cec7b282b0e3d29e2a626b
paaulorickardo <paaulorickardo@hotmail.com>
2021-12-29 08:00:56 -0300

    Aula 02 -Faça um program que leia o nome de uma pessoa e mostre uma mensagem de boas vindas!

---
+++ 002_respondendoAoUsuario.py
@@ -0,0 +1,9 @@
+#Faça um program que leia o nome de uma pessoa e mostre uma mensagem de boas vindas!
+
+#Minha versão
+nome = input('Digite seu nome: ')
+print('bem vindo', nome)
+
+#Versão da aula
+nome = input('Digite seu nome: ')
+print('É um prazer te conhecer {}!'.format(nome))
