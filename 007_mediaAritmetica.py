5b328dfcdde79bbda0da98ddf5f9194b4cc3e713
paaulorickardo <paaulorickardo@hotmail.com>
2021-12-29 08:34:00 -0300

    Aula 07- Desenvolva um programa que leia as duas notas de um aluno, calcule a sua média

---
+++ 007_mediaAritmetica.py
@@ -0,0 +1,8 @@
+# Desenvolva um programa que leia as duas notas de um aluno, calcule a sua média
+
+#Minha versão
+n1 = float(input('Digite a primeira nota: '))
+n2 = float(input('Digite a segunda nota: '))
+print('A média entre a nota {:.1f} e a nota {:.1f} é {:.1f}'.format(n1, n2, (n1 + n2) / 2))
+
+#Versão da aula bem parecida
