c0b80768c32aae093749e7a49db1eab2aa5836b8
Paulo Ricardo <paaulorickardo@hotmail.com>
2022-01-06 14:10:25 -0300

    Aula 35- Analisando Triangulo

---
+++ 035_Analisando_Triangulo.py
@@ -0,0 +1,16 @@
+# #Desenvolva um program que leia o comprimento da três retas e diga ao
+# usuário se elas podem ou não formar um triângulo
+
+#Versão da aula
+
+print('-=' * 20)
+print('Analisador de Triângulos')
+print('-=' * 20)
+r1 = float(input('Primeiro segmento: '))
+r2 = float(input('Segundo segmento: '))
+r3 = float(input('Terceiro segmento: '))
+if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
+    print('Os segmentos acima PODEM FORMAR triângulo! ')
+else:
+    print('Os segmentos acima NÃO PODEM FORMAR trinângulo')
+
