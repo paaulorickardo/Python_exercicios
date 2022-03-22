ff1342749644b7fe83a93fbf913fafa42e286a88
Paulo Ricardo <paaulorickardo@hotmail.com>
2022-03-22 11:18:30 -0300

    Aula 61- Progressao Aritmética v.2.0

---
+++ Progressao_Aritmetica_v2.0.py
@@ -0,0 +1,17 @@
+# Refaça o DESAFIO 051, lendo o primeiro termo e a razão de uma PA,
+# mostrando os 10 primeiros termos da progressao usando a estrutura
+# while
+
+#Versão da aula
+
+print('Gerador de PA')
+print('-=' * 10)
+primeiro = int(input('Razão da PA: '))
+razao = int(input('Razão da PA: '))
+termo = primeiro
+cont = 1
+while cont <= 10:
+    print('{} - '.format(termo), end='')
+    termo += razao
+    cont += 1
+print('FIM')
