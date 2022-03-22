94e99a6571e24f8060fcce3995d01d8e2eb09ad4
Paulo Ricardo <paaulorickardo@hotmail.com>
2022-03-22 11:38:35 -0300

    Aula 63- Sequência de Fibonacci

---
+++ 063_Sequencia_fibonacci_v1.0.py
@@ -0,0 +1,21 @@
+# Escreva um programa que leia um númerio n inteiro qualquer e mostre
+# na tela os n primeiros elementos de uma Sequência de Fibonacci
+
+#Versão da aula
+
+print('-'*30)
+print('Sequência de Fibonacci')
+print('-'*30)
+n = int(input('Quantos termos você quer mostrar? '))
+t1 = 0
+t2 = 1
+print('~'*30)
+print('{} - {}'.format(t1, t2), end='')
+cont = 3
+while cont <= n:
+    t3 = t1 + t2
+    print('- {}'.format(t3), end= '')
+    t1 = t2
+    t2 = t3
+    cont += 1
+print(' - FIM')
