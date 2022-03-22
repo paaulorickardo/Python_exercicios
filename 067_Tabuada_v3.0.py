b9565abf322cfd47a0437eb83674de2c8d7e1df9
Paulo Ricardo <paaulorickardo@hotmail.com>
2022-03-22 13:52:41 -0300

    Aula 67- Tabuada v3.0

---
+++ 067_Tabuada_v3.0.py
@@ -0,0 +1,14 @@
+# Faça um programa que mostre a tabuada de vários numeros, um de
+# cada vez, para cada valor digitado pelo usuário. O programa será interrompido
+# quando o número solicitado for negativo.
+
+#Versão da aula
+while True:
+    n = int(input('Quer ver a tabuada de qual valor? '))
+    if n < 0:
+        break
+    print('-' * 30)
+    for c in range(1, 11):
+        print(f'{n} x {c} = {n*c}')
+    print('-' * 30)
+print('PROGRAMA DE TABUADA ENCERRADO')
