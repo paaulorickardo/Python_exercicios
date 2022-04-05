496f1544a3416f04fea7973f65f5574a1700bda2
Paulo Ricardo <paaulorickardo@hotmail.com>
2022-04-05 11:23:33 -0300

    Aula 70- Estatisticas em produtos

---
+++ 070_Estaticas_Produtos.py
@@ -0,0 +1,29 @@
+# Crie um programa que leia o nome e o preço de vários produtos. O programa
+# deverá perguntar se o usuário vai continuar. No final, mostre:
+# a) Qual é o total gasto na compra
+# b) Quantos produtos custam mais de R$1.000
+# C) Qual é o nome do produto mais barato
+
+#Versão da aula
+
+total = totmil = menor = cont = 0
+barato = ''
+while True:
+    produto = str(input('Nome do Produto: '))
+    preco = float(input('Preço: R$ '))
+    cont += 1
+    total += preco
+    if preco > 1000:
+        totmil += 1
+    if cont == 1 or preco < menor:
+        menor = preco
+        barato = produto
+    resp = ' '
+    while resp not in 'SN':
+        resp = str(input('Quer continuar? [S/N]')).strip().upper()[0]
+    if resp == 'N':
+        break
+print('{:-^40}'.format('FIM DO PROGRAMA'))
+print(f'O total da compra foi R${total:.2f}')
+print(f'Temos {totmil} produtos custando mais de R$1000.00')
+print(f'O produto mais barato foi {barato} que custa R${menor:.2f}')
